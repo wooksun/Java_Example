50e5b6f7c3cb4c964ae8290912cbcdba2449982d
wooksun <uk1231@naver.com>
2023-04-14 17:00:51 +0900

    @2차원 배열의 달팽이 예제 시계방향

---
+++ k20230414/src/k20230414/SnailTest.java
@@ -0,0 +1,47 @@
+package k20230414;
+
+import java.util.Scanner;
+
+public class SnailTest {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("행렬의 차수를 입력하세요: ");
+		int row = sc.nextInt();
+
+		int[][] a = new int[row][row];
+		int n = 0; //배열에 채워질 숫자를 1씩 증가시킬 변수
+		int s = 1; //행과 열이 1또는 -1씩 증가 및 감소 처리에 사용할 변수
+		int i = 0; // 행
+		int j = -1; // 열
+		int k = row; // 반복문의 반복 횟수 제어에 사용하는 변수
+		
+		while(true) {
+			for(int p=1; p<=k; p++) { //행 방향 숫자 채우기
+				j += s;
+				a[i][j] = ++n;
+			}
+			if(--k <= 0) {
+				break;
+			}
+			for(int p=1; p<=k; p++) {// 열 방향 숫자 채우기
+				i += s;
+				a[i][j] = ++n;
+			}
+			s *= -1; //부호 변경
+		}
+		
+		for(i=0; i<a.length; i++) { //출력하는 코드
+			for(j=0; j<a[i].length; j++) {
+				System.out.printf("%3d ", a[i][j]);
+			}	
+			System.out.println();
+		}
+		
+		
+		
+
+	}
+
+}
